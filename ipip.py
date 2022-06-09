e35939497c4ce3a1c0c0bf749a687729c4d88c5c
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2022-06-09 21:25:43 +0800

    Update ipip.py

--- ipip.py
+++ ipip.py
@@ -37,7 +37,7 @@ tpe = ""
 for i in temp_lists:
     if tep != i[1]:
         temps.append((i[1], temp_lists.count(i)))
-        tpe = i
+        tpe = i[1]
     tep = i[1]
 if tpe != temps[-1][0]:
     temps.append((temp_lists[-1][1], temp_lists.count(temp_lists[-1])))
