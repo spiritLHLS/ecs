6cf48d6d571048ecb4bb788fcebab0ae3fd203f6
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2022-06-09 18:51:01 +0800

    Update ecs.sh

--- ecs.sh
+++ ecs.sh
@@ -347,8 +347,8 @@ Run_SysBench_CPU() {
 
 Function_SysBench_CPU_Fast() {
     mkdir -p ${WorkDir}/SysBench/CPU/ >/dev/null 2>&1
-    echo -e "\n ${Font_Yellow}-> CPU 测试中 (Fast Mode, 1-Pass @ 5sec)${Font_Suffix}\n"
-    echo -e "\n -> CPU 测试中 (Fast Mode, 1-Pass @ 5sec)\n" >>${WorkDir}/SysBench/CPU/result.txt
+    echo -e " ${Font_Yellow}-> CPU 测试中 (Fast Mode, 1-Pass @ 5sec)${Font_Suffix}\n"
+    echo -e " -> CPU 测试中 (Fast Mode, 1-Pass @ 5sec)\n" >>${WorkDir}/SysBench/CPU/result.txt
     Run_SysBench_CPU "1" "5" "1" "1 线程测试(1核)得分"
     if [ "${LBench_Result_CPUThreadNumber}" -ge "2" ]; then
         Run_SysBench_CPU "${LBench_Result_CPUThreadNumber}" "5" "1" "${LBench_Result_CPUThreadNumber} 线程测试(多核)得分"
@@ -357,7 +357,6 @@ Function_SysBench_CPU_Fast() {
     fi
     # 完成FLAG
     LBench_Flag_FinishSysBenchCPUFast="1"
-    echo -e ""
 }
 
 
@@ -1266,8 +1265,103 @@ Function_DiskTest_Fast() {
     Run_DiskTest_DD "1GB.test" "1M" "1000" "1GB-1M Block"
     # 执行完成, 标记FLAG
     LBench_Flag_FinishDiskTestFast="1"
-    echo -e ""
     sleep 1
+}
+
+# =============== SysBench - 内存性能 部分 ===============
+Run_SysBench_Memory() {
+    # 调用方式: Run_SysBench_Memory "线程数" "测试时长(s)" "测试遍数" "测试模式(读/写)" "读写方式(顺序/随机)" "说明"
+    # 变量初始化
+    mkdir -p ${WorkDir}/SysBench/Memory/ >/dev/null 2>&1
+    maxtestcount="$3"
+    local count="1"
+    local TestScore="0.00"
+    local TestSpeed="0.00"
+    local TotalScore="0.00"
+    local TotalSpeed="0.00"
+    if [ "$1" -ge "2" ]; then
+        MultiThread_Flag="1"
+    else
+        MultiThread_Flag="0"
+    fi
+    # 运行测试
+    while [ $count -le $maxtestcount ]; do
+        if [ "$1" -ge "2" ] && [ "$4" = "write" ]; then
+            echo -e "\r ${Font_Yellow}$6:${Font_Suffix}\t$count/$maxtestcount \c"
+        else
+            echo -e "\r ${Font_Yellow}$6:${Font_Suffix}\t\t$count/$maxtestcount \c"
+        fi
+        local TestResult="$(sysbench --test=memory --num-threads=$1 --memory-block-size=1M --memory-total-size=102400G --memory-oper=$4 --max-time=$2 --memory-access-mode=$5 run 2>&1)"
+        # 判断是MB还是MiB
+        echo "${TestResult}" | grep -oE "MiB" >/dev/null 2>&1
+        if [ $? -eq 0 ]; then
+            local MiB_Flag="1"
+        else
+            local MiB_Flag="0"
+        fi
+        local TestScore="$(echo "${TestResult}" | grep -oE "[0-9]{1,}.[0-9]{1,2} ops\/sec|[0-9]{1,}.[0-9]{1,2} per second" | grep -oE "[0-9]{1,}.[0-9]{1,2}")"
+        local TestSpeed="$(echo "${TestResult}" | grep -oE "[0-9]{1,}.[0-9]{1,2} MB\/sec|[0-9]{1,}.[0-9]{1,2} MiB\/sec" | grep -oE "[0-9]{1,}.[0-9]{1,2}")"
+        local TotalScore="$(echo "${TotalScore} ${TestScore}" | awk '{printf "%.2f",$1+$2}')"
+        local TotalSpeed="$(echo "${TotalSpeed} ${TestSpeed}" | awk '{printf "%.2f",$1+$2}')"
+        let count=count+1
+        local TestResult=""
+        local TestScore="0.00"
+        local TestSpeed="0.00"
+    done
+    ResultScore="$(echo "${TotalScore} ${maxtestcount} 1000" | awk '{printf "%.2f",$1/$2/$3}')"
+    if [ "${MiB_Flag}" = "1" ]; then
+        # MiB to MB
+        ResultSpeed="$(echo "${TotalSpeed} ${maxtestcount} 1048576‬ 1000000" | awk '{printf "%.2f",$1/$2/$3*$4}')"
+    else
+        # 直接输出
+        ResultSpeed="$(echo "${TotalSpeed} ${maxtestcount}" | awk '{printf "%.2f",$1/$2}')"
+    fi
+    # 1线程的测试结果写入临时变量，方便与后续的多线程变量做对比
+    if [ "$1" = "1" ] && [ "$4" = "read" ]; then
+        LBench_Result_MemoryReadSpeedSingle="${ResultSpeed}"
+    elif [ "$1" = "1" ] &&[ "$4" = "write" ]; then
+        LBench_Result_MemoryWriteSpeedSingle="${ResultSpeed}"
+    fi
+    if [ "${MultiThread_Flag}" = "1" ]; then
+        # 如果是多线程测试，输出与1线程测试对比的倍率
+        if [ "$1" -ge "2" ] && [ "$4" = "read" ]; then
+            LBench_Result_MemoryReadSpeedMulti="${ResultSpeed}"
+            local readmultiple="$(echo "${LBench_Result_MemoryReadSpeedMulti} ${LBench_Result_MemoryReadSpeedSingle}" | awk '{printf "%.2f", $1/$2}')"
+            echo -e "\r ${Font_Yellow}$6:${Font_Suffix}\t\t${Font_SkyBlue}${LBench_Result_MemoryReadSpeedMulti}${Font_Suffix} ${Font_Yellow}MB/s${Font_Suffix} (${readmultiple} x)"
+        elif [ "$1" -ge "2" ] && [ "$4" = "write" ]; then
+            LBench_Result_MemoryWriteSpeedMulti="${ResultSpeed}"
+            local writemultiple="$(echo "${LBench_Result_MemoryWriteSpeedMulti} ${LBench_Result_MemoryWriteSpeedSingle}" | awk '{printf "%.2f", $1/$2}')"
+            echo -e "\r ${Font_Yellow}$6:${Font_Suffix}\t\t${Font_SkyBlue}${LBench_Result_MemoryWriteSpeedMulti}${Font_Suffix} ${Font_Yellow}MB/s${Font_Suffix} (${writemultiple} x)"
+        fi
+    else
+        if [ "$4" = "read" ]; then
+            echo -e "\r ${Font_Yellow}$6:${Font_Suffix}\t\t${Font_SkyBlue}${ResultSpeed}${Font_Suffix} ${Font_Yellow}MB/s${Font_Suffix}"
+        elif [ "$4" = "write" ]; then
+            echo -e "\r ${Font_Yellow}$6:${Font_Suffix}\t\t${Font_SkyBlue}${ResultSpeed}${Font_Suffix} ${Font_Yellow}MB/s${Font_Suffix}"
+        fi
+    fi
+    # Fix
+    if [ "$1" -ge "2" ] && [ "$4" = "write" ]; then
+        echo -e " $6:\t${ResultSpeed} MB/s" >>${WorkDir}/SysBench/Memory/result.txt
+    else
+        echo -e " $6:\t\t${ResultSpeed} MB/s" >>${WorkDir}/SysBench/Memory/result.txt
+    fi
+    sleep 1
+}
+
+Function_SysBench_Memory_Fast() {
+    mkdir -p ${WorkDir}/SysBench/Memory/ >/dev/null 2>&1
+    echo -e " ${Font_Yellow}-> 内存测试 Test (Fast Mode, 1-Pass @ 5sec)${Font_Suffix}\n"
+    echo -e " -> 内存测试 (Fast Mode, 1-Pass @ 5sec)\n" >>${WorkDir}/SysBench/Memory/result.txt
+    Run_SysBench_Memory "1" "5" "1" "read" "seq" "单线程读测试"
+    Run_SysBench_Memory "1" "5" "1" "write" "seq" "单线程写测试"
+    # 完成FLAG
+    LBench_Flag_FinishSysBenchMemoryFast="1"
+    sleep 1
+}
+
+Entrance_SysBench_Memory_Fast() {
+    Function_SysBench_Memory_Fast
 }
 
 Entrance_DiskTest_Fast() {
@@ -1537,6 +1631,8 @@ print_system_info
 ipv4_info
 echo "-------------------CPU测试--感谢lemonbench开源------------------------"
 Entrance_SysBench_CPU_Fast
+echo "-------------------内存测试--感谢lemonbench开源-----------------------"
+Entrance_SysBench_Memory_Fast
 echo "----------------磁盘IO读写测试--感谢lemonbench开源--------------------"
 #print_io_test
 Entrance_DiskTest_Fast
@@ -1574,6 +1670,5 @@ rm -rf ipip.py
 rm -rf dp
 rm -rf nf
 rm -rf tubecheck
-rm -rf speedtest.tgz
 rm -rf ecs.sh
 
