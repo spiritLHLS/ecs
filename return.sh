a68f4ea2e1fca4cfbcfb7714005e3956ff3f044b
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2022-06-10 10:09:34 +0800

    Update return.sh

--- return.sh
+++ return.sh
@@ -13,6 +13,6 @@ esac
 ip=$1
 green " 本脚说明：测 VPS ——> 对端 经过的地区及线路，填本地IP就是测回程，核心程序来由: https://www.ipip.net/ ，请知悉！"
 [[ -z "$ip" || $ip = '[DESTINATION_IP]' ]] && reading " 请输入目的地 IP: " ip
-[[ ! -e "$FILE" ]] && green " 下载 IPIP.net 测线路文件 " && wget https://cdn.jsdelivr.net/gh/fscarmen/tools/besttrace/$FILE && chmod 777 return.sh
+[[ ! -e "$FILE" ]] && green " 下载 IPIP.net 测线路文件 " && wget https://github.com/fscarmen/tools/raw/main/besttrace/$FILE && chmod 777 return.sh
 chmod +x "$FILE" >/dev/null 2>&1
 sudo ./"$FILE" "$ip" -g cn
