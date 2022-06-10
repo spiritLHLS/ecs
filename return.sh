cb4c20b461d0f6086e93efa6dc7069035c74151b
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2022-06-10 23:31:34 +0800

    Update return.sh

--- return.sh
+++ return.sh
@@ -10,9 +10,7 @@ i386 )    FILE=besttracemac;;
 * ) red " 只支持 AMD64、ARM64、Mac 使用，问题反馈:[https://github.com/fscarmen/tools/issues] " && exit 1;;
 esac
 
-ip=$1
 # green " 本脚说明：测 VPS ——> 对端 经过的地区及线路，填本地IP就是测回程，核心程序来由: https://www.ipip.net/ ，请知悉！"
-[[ -z "$ip" || $ip = '[DESTINATION_IP]' ]] >/dev/null 2>&1 && reading " 请输入目的地 IP: " ip
 wget https://github.com/fscarmen/tools/raw/main/besttrace/besttrace >/dev/null 2>&1
 chmod 777 besttrace >/dev/null 2>&1
 wget https://github.com/fscarmen/tools/raw/main/besttrace/besttracearm >/dev/null 2>&1 
@@ -21,7 +19,7 @@ chmod 777 besttracearm >/dev/null 2>&1
 # chmod 777 besttracemac >/dev/null 2>&1
 [[ ! -e "$FILE" ]] >/dev/null 2>&1 && wget https://github.com/fscarmen/tools/raw/main/besttrace/$FILE >/dev/null 2>&1 && chmod 777 $FILE >/dev/null 2>&1
 chmod +x "$FILE" >/dev/null 2>&1
-sudo ./"$FILE" "$ip" -g cn
+sudo ./"$FILE" "$1" -g cn
 # rm -rf besttracemac
 rm -rf besttracearm >/dev/null 2>&1
 rm -rf besttrace >/dev/null 2>&1
