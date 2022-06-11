44f38453cb4001e6840e48cc9f47752a579a1f77
fscarmen <62703343+fscarmen@users.noreply.github.com>
2022-06-11 23:09:15 +0800

    处理用了warp之后，局域网显示的错误

--- return.sh
+++ return.sh
@@ -16,5 +16,5 @@ esac
 [[ ! -e $FILE ]] && wget -q https://github.com/fscarmen/tools/raw/main/besttrace/$FILE >/dev/null 2>&1
 chmod 777 $FILE >/dev/null 2>&1
 sudo ./"$FILE" "$1" -g cn > $TEMP_FILE
-green "$(cat $TEMP_FILE | sed "s/.*AS[0-9]*//g" | sed "/\*$/d;/^$/d;1d" | uniq | awk '{printf("%d,%s\n"),NR,$0}')"
+green "$(cat $TEMP_FILE | sed "s/.*\*\(.*局域网\)/\1/g" | sed "s/.*AS[0-9]*//g" | sed "/\*$/d;/^$/d;1d" | uniq | awk '{printf("%d.%s\n"),NR,$0}')"
 rm -f $TEMP_FILE
