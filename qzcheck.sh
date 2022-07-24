2ed4f78ccbab654f86aa9c91e943933ba8268537
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2022-07-24 09:55:42 +0800

    Update qzcheck.sh

--- qzcheck.sh
+++ qzcheck.sh
@@ -155,8 +155,8 @@ print_end_time() {
 
 checkpython() {
     ! type -p python3 >/dev/null 2>&1 && yellow "\n Install python3\n" && ${PACKAGE_INSTALL[int]} python3
-    # ! type -p pip3 install subprocess >/dev/null 2>&1 && yellow "\n Install pip3\n" && ${PACKAGE_INSTALL[int]} python3-pip
-    # pip3 install subprocess
+    ! type -p pip3 install subprocess >/dev/null 2>&1 && yellow "\n Install pip3\n" && ${PACKAGE_INSTALL[int]} python3-pip
+    pip3 install requests
     sleep 0.5
 }
 
