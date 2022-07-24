4a395700336964f01ba2b1ad37d1a5433fbd5438
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2022-07-24 10:27:10 +0800

    Update qzcheck.py

--- qzcheck.py
+++ qzcheck.py
@@ -2,6 +2,7 @@ import requests
 import subprocess
 import re
 
+
 def excuteCommand(com):
     ex = subprocess.Popen(com, stdout=subprocess.PIPE, shell=True)
     out, err = ex.communicate()
@@ -9,6 +10,7 @@ def excuteCommand(com):
     # print("cmd in:", com)
     # print("cmd out: ", out.decode())
     return out.decode()
+
 
 ip = excuteCommand("curl -sm8 ip.sb").replace("\n", "").replace(" ", "")
 context = requests.get(f"https://scamalytics.com/ip/{ip}", timeout=30).text
@@ -19,13 +21,19 @@ nlist = ["匿名代理", "Tor出口节点", "服务器IP", "公共代理", "网�
 for i, j in zip(temp2, nlist):
     temp3 = re.findall(f"\">(.*?)</", i)[0]
     print(f"{j}: {temp3}")
-for i in range(0, 101):
+for i in range(1, 101):
     try:
-        context1 = requests.get(f"https://cf-threat.sukkaw.com/hello.json?threat={str(i)}", timeout=1).json["ping"]
+        context1 = requests.get(
+            f"https://cf-threat.sukkaw.com/hello.json?threat={str(i)}",
+            timeout=1).text
         if "pong!" not in context1:
-            print("0判定为低风险,高于10判定为爬虫或者垃圾邮件发送者,高于40判定为有严重不良行为的IP(如僵尸网络等),这个数值一般不会大于60")
-            print("Cloudflare威胁得分：", context1)
+            print(
+                "0判定为低风险,高于10判定为爬虫或者垃圾邮件发送者,高于40判定为有严重不良行为的IP(如僵尸网络等),这个数值一般不会大于60"
+            )
+            print("Cloudflare威胁得分：", str(i))
             break
     except:
         pass
-        
+if i == 100:
+    print("0判定为低风险,高于10判定为爬虫或者垃圾邮件发送者,高于40判定为有严重不良行为的IP(如僵尸网络等),这个数值一般不会大于60")
+    print("Cloudflare威胁得分: 0")
