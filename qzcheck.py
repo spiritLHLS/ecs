629f9cdbe833ce0f08d1ebe771d78dc5ec537797
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2022-07-25 10:10:13 +0800

    Update qzcheck.py

--- qzcheck.py
+++ qzcheck.py
@@ -89,7 +89,6 @@ except Exception as e:
 try:
     with open("gdlog", "r") as fp:
         context3 = fp.read()
-    print(context3)
     if "https://www.spiritysdx.top/" in context3:
         print("Google搜索可行性：yes")
     else:
