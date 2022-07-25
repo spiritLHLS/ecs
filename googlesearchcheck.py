ecc95c00b832f01ee537e776b022fb99f597f901
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2022-07-25 10:05:53 +0800

    Update googlesearchcheck.py

--- googlesearchcheck.py
+++ googlesearchcheck.py
@@ -1,6 +1,6 @@
 from magic_google import MagicGoogle
 
 mg = MagicGoogle()
-for url in mg.search_url(query='二叉树的博客', num=1, language="en"):
+for url in mg.search_url(query='二叉树的博客', num=3, language="en"):
     with open("gdlog", "w") as fp:
         fp.write(url)
