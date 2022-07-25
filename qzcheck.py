c4765cb312284c65394c5871da1d94555efbd14d
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2022-07-25 09:56:16 +0800

    Update qzcheck.py

--- qzcheck.py
+++ qzcheck.py
@@ -87,7 +87,7 @@ except Exception as e:
     print(e)
 
 try:
-    with open("gdlog.txt", "r", encoding="utf-8") as fp:
+    with open("./gdlog.txt", "r", encoding="utf-8") as fp:
         context3 = fp.read()
     print(context3)
     if "https://www.spiritysdx.top/" in context3:
