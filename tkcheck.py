6f7be30f34b5f13bc9be05d36dd0a7b47499b99d
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2022-07-28 11:36:49 +0800

    Update tkcheck.py

--- tkcheck.py
+++ tkcheck.py
@@ -11,5 +11,8 @@ def excuteCommand(com):
     return out.decode()
 
 context = excuteCommand("curl -fsL -o ./t.sh.x https://github.com/lmc999/TikTokCheck/raw/main/t.sh.x && chmod +x ./t.sh.x && ./t.sh.x && rm ./t.sh.x").split("\n")
-context1 = [i for i in context if "Tiktok Region" in i][0].replace(" ","").split(":")[2].replace("\t","")
+try:
+    context1 = [i for i in context if "Tiktok Region" in i][0].replace(" ","").split(":")[2].replace("\t","")
+except:
+    context1 = "NO"
 print("TikTok解锁区域：", context1)
