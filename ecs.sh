be8f61a37ab42d459e799ef6b58e3b22ff5857aa
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2022-08-14 19:05:08 +0800

    Update ecs.sh

--- ecs.sh
+++ ecs.sh
@@ -53,6 +53,7 @@ trap "Global_TrapSigExit_Sig15" 15
 Global_TrapSigExit_Sig1() {
     echo -e "\n\n${Msg_Error}Caught Signal SIGHUP, Exiting ...\n"
     Global_TrapSigExit_Action
+    rm_script
     exit 1
 }
 
@@ -60,6 +61,7 @@ Global_TrapSigExit_Sig1() {
 Global_TrapSigExit_Sig2() {
     echo -e "\n\n${Msg_Error}Caught Signal SIGINT (or Ctrl+C), Exiting ...\n"
     Global_TrapSigExit_Action
+    rm_script
     exit 1
 }
 
@@ -67,6 +69,7 @@ Global_TrapSigExit_Sig2() {
 Global_TrapSigExit_Sig3() {
     echo -e "\n\n${Msg_Error}Caught Signal SIGQUIT, Exiting ...\n"
     Global_TrapSigExit_Action
+    rm_script
     exit 1
 }
 
@@ -74,6 +77,7 @@ Global_TrapSigExit_Sig3() {
 Global_TrapSigExit_Sig15() {
     echo -e "\n\n${Msg_Error}Caught Signal SIGTERM, Exiting ...\n"
     Global_TrapSigExit_Action
+    rm_script
     exit 1
 }
 
@@ -108,12 +112,10 @@ Global_StartupInit_Action() {
 Global_TrapSigExit_Action() {
     rm -rf ${WorkDir}
     rm -rf /.tmp_LBench/
-    rm_script
 }
 
 Global_Exit_Action() {
     rm -rf ${WorkDir}/
-    rm_script
 }
 
 Function_BenchFinish() {
@@ -696,133 +698,6 @@ SystemInfo_GetSystemBit() {
     fi
 }
 
-
-# SystemInfo_GetMemInfo() {
-#     mkdir -p ${WorkDir}/data >/dev/null 2>&1
-#     cat /proc/meminfo >${WorkDir}/data/meminfo
-#     local ReadMemInfo="cat ${WorkDir}/data/meminfo"
-#     # 获取总内存
-#     LBench_Result_MemoryTotal_KB="$($ReadMemInfo | awk '/MemTotal/{print $2}')"
-#     LBench_Result_MemoryTotal_MB="$(echo $LBench_Result_MemoryTotal_KB | awk '{printf "%.2f\n",$1/1024}')"
-#     LBench_Result_MemoryTotal_GB="$(echo $LBench_Result_MemoryTotal_KB | awk '{printf "%.2f\n",$1/1048576}')"
-#     # 获取可用内存
-#     local MemFree="$($ReadMemInfo | awk '/MemFree/{print $2}')"
-#     local Buffers="$($ReadMemInfo | awk '/Buffers/{print $2}')"
-#     local Cached="$($ReadMemInfo | awk '/Cached/{print $2}')"
-#     LBench_Result_MemoryFree_KB="$(echo $MemFree $Buffers $Cached | awk '{printf $1+$2+$3}')"
-#     LBench_Result_MemoryFree_MB="$(echo $LBench_Result_MemoryFree_KB | awk '{printf "%.2f\n",$1/1024}')"
-#     LBench_Result_MemoryFree_GB="$(echo $LBench_Result_MemoryFree_KB | awk '{printf "%.2f\n",$1/1048576}')"
-#     # 获取已用内存
-#     local MemUsed="$(echo $LBench_Result_MemoryTotal_KB $LBench_Result_MemoryFree_KB | awk '{printf $1-$2}')"
-#     LBench_Result_MemoryUsed_KB="$MemUsed"
-#     LBench_Result_MemoryUsed_MB="$(echo $LBench_Result_MemoryUsed_KB | awk '{printf "%.2f\n",$1/1024}')"
-#     LBench_Result_MemoryUsed_GB="$(echo $LBench_Result_MemoryUsed_KB | awk '{printf "%.2f\n",$1/1048576}')"
-#     # 获取Swap总量
-#     LBench_Result_SwapTotal_KB="$($ReadMemInfo | awk '/SwapTotal/{print $2}')"
-#     LBench_Result_SwapTotal_MB="$(echo $LBench_Result_SwapTotal_KB | awk '{printf "%.2f\n",$1/1024}')"
-#     LBench_Result_SwapTotal_GB="$(echo $LBench_Result_SwapTotal_KB | awk '{printf "%.2f\n",$1/1048576}')"
-#     # 获取可用Swap
-#     LBench_Result_SwapFree_KB="$($ReadMemInfo | awk '/SwapFree/{print $2}')"
-#     LBench_Result_SwapFree_MB="$(echo $LBench_Result_SwapFree_KB | awk '{printf "%.2f\n",$1/1024}')"
-#     LBench_Result_SwapFree_GB="$(echo $LBench_Result_SwapFree_KB | awk '{printf "%.2f\n",$1/1048576}')"
-#     # 获取已用Swap
-#     local SwapUsed="$(echo $LBench_Result_SwapTotal_KB $LBench_Result_SwapFree_KB | awk '{printf $1-$2}')"
-#     LBench_Result_SwapUsed_KB="$SwapUsed"
-#     LBench_Result_SwapUsed_MB="$(echo $LBench_Result_SwapUsed_KB | awk '{printf "%.2f\n",$1/1024}')"
-#     LBench_Result_SwapUsed_GB="$(echo $LBench_Result_SwapUsed_KB | awk '{printf "%.2f\n",$1/1048576}')"
-# }
-
-
-
-# SystemInfo_GetOSRelease() {
-#     if [ -f "/etc/centos-release" ]; then # CentOS
-#         Var_OSRelease="centos"
-#         local Var_OSReleaseFullName="$(cat /etc/os-release | awk -F '[= "]' '/PRETTY_NAME/{print $3,$4}')"
-#         if [ "$(rpm -qa | grep -o el6 | sort -u)" = "el6" ]; then
-#             Var_CentOSELRepoVersion="6"
-#             local Var_OSReleaseVersion="$(cat /etc/centos-release | awk '{print $3}')"
-#         elif [ "$(rpm -qa | grep -o el7 | sort -u)" = "el7" ]; then
-#             Var_CentOSELRepoVersion="7"
-#             local Var_OSReleaseVersion="$(cat /etc/centos-release | awk '{print $4}')"
-#         elif [ "$(rpm -qa | grep -o el8 | sort -u)" = "el8" ]; then
-#             Var_CentOSELRepoVersion="8"
-#             local Var_OSReleaseVersion="$(cat /etc/centos-release | awk '{print $4}')"
-#         else
-#             local Var_CentOSELRepoVersion="unknown"
-#             local Var_OSReleaseVersion="<Unknown Release>"
-#         fi
-#         local Var_OSReleaseArch="$(arch)"
-#         LBench_Result_OSReleaseFullName="$Var_OSReleaseFullName $Var_OSReleaseVersion ($Var_OSReleaseArch)"
-#     elif [ -f "/etc/redhat-release" ]; then # RedHat
-#         Var_OSRelease="rhel"
-#         local Var_OSReleaseFullName="$(cat /etc/os-release | awk -F '[= "]' '/PRETTY_NAME/{print $3,$4}')"
-#         if [ "$(rpm -qa | grep -o el6 | sort -u)" = "el6" ]; then
-#             Var_RedHatELRepoVersion="6"
-#             local Var_OSReleaseVersion="$(cat /etc/redhat-release | awk '{print $3}')"
-#         elif [ "$(rpm -qa | grep -o el7 | sort -u)" = "el7" ]; then
-#             Var_RedHatELRepoVersion="7"
-#             local Var_OSReleaseVersion="$(cat /etc/redhat-release | awk '{print $4}')"
-#         elif [ "$(rpm -qa | grep -o el8 | sort -u)" = "el8" ]; then
-#             Var_RedHatELRepoVersion="8"
-#             local Var_OSReleaseVersion="$(cat /etc/redhat-release | awk '{print $4}')"
-#         else
-#             local Var_RedHatELRepoVersion="unknown"
-#             local Var_OSReleaseVersion="<Unknown Release>"
-#         fi
-#         local Var_OSReleaseArch="$(arch)"
-#         LBench_Result_OSReleaseFullName="$Var_OSReleaseFullName $Var_OSReleaseVersion ($Var_OSReleaseArch)"
-#     elif [ -f "/etc/fedora-release" ]; then # Fedora
-#         Var_OSRelease="fedora"
-#         local Var_OSReleaseFullName="$(cat /etc/os-release | awk -F '[= "]' '/PRETTY_NAME/{print $3}')"
-#         local Var_OSReleaseVersion="$(cat /etc/fedora-release | awk '{print $3,$4,$5,$6,$7}')"
-#         local Var_OSReleaseArch="$(arch)"
-#         LBench_Result_OSReleaseFullName="$Var_OSReleaseFullName $Var_OSReleaseVersion ($Var_OSReleaseArch)"
-#     elif [ -f "/etc/lsb-release" ]; then # Ubuntu
-#         Var_OSRelease="ubuntu"
-#         local Var_OSReleaseFullName="$(cat /etc/os-release | awk -F '[= "]' '/NAME/{print $3}' | head -n1)"
-#         local Var_OSReleaseVersion="$(cat /etc/os-release | awk -F '[= "]' '/VERSION/{print $3,$4,$5,$6,$7}' | head -n1)"
-#         local Var_OSReleaseArch="$(arch)"
-#         LBench_Result_OSReleaseFullName="$Var_OSReleaseFullName $Var_OSReleaseVersion ($Var_OSReleaseArch)"
-#         Var_OSReleaseVersion_Short="$(cat /etc/lsb-release | awk -F '[= "]' '/DISTRIB_RELEASE/{print $2}')"
-#     elif [ -f "/etc/debian_version" ]; then # Debian
-#         Var_OSRelease="debian"
-#         local Var_OSReleaseFullName="$(cat /etc/os-release | awk -F '[= "]' '/PRETTY_NAME/{print $3,$4}')"
-#         local Var_OSReleaseVersion="$(cat /etc/debian_version | awk '{print $1}')"
-#         local Var_OSReleaseVersionShort="$(cat /etc/debian_version | awk '{printf "%d\n",$1}')"
-#         if [ "${Var_OSReleaseVersionShort}" = "7" ]; then
-#             Var_OSReleaseVersion_Short="7"
-#             Var_OSReleaseVersion_Codename="wheezy"
-#             local Var_OSReleaseFullName="${Var_OSReleaseFullName} \"Wheezy\""
-#         elif [ "${Var_OSReleaseVersionShort}" = "8" ]; then
-#             Var_OSReleaseVersion_Short="8"
-#             Var_OSReleaseVersion_Codename="jessie"
-#             local Var_OSReleaseFullName="${Var_OSReleaseFullName} \"Jessie\""
-#         elif [ "${Var_OSReleaseVersionShort}" = "9" ]; then
-#             Var_OSReleaseVersion_Short="9"
-#             Var_OSReleaseVersion_Codename="stretch"
-#             local Var_OSReleaseFullName="${Var_OSReleaseFullName} \"Stretch\""
-#         elif [ "${Var_OSReleaseVersionShort}" = "10" ]; then
-#             Var_OSReleaseVersion_Short="10"
-#             Var_OSReleaseVersion_Codename="buster"
-#             local Var_OSReleaseFullName="${Var_OSReleaseFullName} \"Buster\""
-#         else
-#             Var_OSReleaseVersion_Short="sid"
-#             Var_OSReleaseVersion_Codename="sid"
-#             local Var_OSReleaseFullName="${Var_OSReleaseFullName} \"Sid (Testing)\""
-#         fi
-#         local Var_OSReleaseArch="$(arch)"
-#         LBench_Result_OSReleaseFullName="$Var_OSReleaseFullName $Var_OSReleaseVersion ($Var_OSReleaseArch)"
-#     elif [ -f "/etc/alpine-release" ]; then # Alpine Linux
-#         Var_OSRelease="alpinelinux"
-#         local Var_OSReleaseFullName="$(cat /etc/os-release | awk -F '[= "]' '/NAME/{print $3,$4}' | head -n1)"
-#         local Var_OSReleaseVersion="$(cat /etc/alpine-release | awk '{print $1}')"
-#         local Var_OSReleaseArch="$(arch)"
-#         LBench_Result_OSReleaseFullName="$Var_OSReleaseFullName $Var_OSReleaseVersion ($Var_OSReleaseArch)"
-#     else
-#         Var_OSRelease="unknown" # 未知系统分支
-#         LBench_Result_OSReleaseFullName="[Error: Unknown Linux Branch !]"
-#     fi
-# }
 
 SystemInfo_GetVirtType() {
     if [ -f "/usr/bin/systemd-detect-virt" ]; then
