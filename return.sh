20fda278a7554e8264b5402108af4567184eac7e
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-01-03 17:54:26 +0800

    Update return.sh

--- return.sh
+++ return.sh
@@ -12,8 +12,7 @@ case $ARCHITECTURE in
   * ) red " 只支持 AMD64、ARM64、Mac 使用，问题反馈:[https://github.com/fscarmen/tools/issues] " && exit 1;;
 esac
 
-[[ ! -e $FILE ]] && wget -q https://github.com/fscarmen/tools/raw/main/besttrace/$FILE >/dev/null 2>&1
-chmod 777 $FILE >/dev/null 2>&1
+curl -s -L -k https://github.com/fscarmen/tools/raw/main/besttrace/$FILE -o $FILE && chmod +x $FILE &>/dev/null
 ./"$FILE" "$1" -g cn > $TEMP_FILE
 green "$(cat $TEMP_FILE | cut -d \* -f2 | sed "s/.*\(  AS[0-9]\)/\1/" | sed "/\*$/d;/^$/d;1d" | uniq | awk '{printf("%d.%s\n"),NR,$0}')"
 rm -f $TEMP_FILE
