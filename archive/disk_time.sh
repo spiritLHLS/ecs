7aa38014647b500569545f52f2e74fa48ad6123d
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-01-16 19:48:35 +0800

    Update disk_time.sh

--- archive/disk_time.sh
+++ archive/disk_time.sh
@@ -2,7 +2,6 @@
 # by https://github.com/spiritLHLS/ecs
 
 # 检测所有硬盘通电时长
-
 disk_list=$(ls /dev/sd* | grep -v [0-9])
 
 #检测smartctl是否安装
@@ -10,10 +9,8 @@ if ! command -v smartctl &> /dev/null
 then
     echo "smartctl not found, installing smartctl..."
     if [ -f /etc/redhat-release ] || [ -f /etc/centos-release ]; then
-        yum update
         yum install smartmontools -y
     elif [ -f /etc/debian_version ]; then
-        apt-get update
         apt-get install smartmontools -y
     elif [ -f /etc/fedora-release ]; then
         dnf install smartmontools -y
@@ -29,7 +26,14 @@ fi
 
 for disk_dev in $disk_list
 do
-    power_on_hours=$(smartctl -A $disk_dev | grep "Power_On_Hours" | awk '{print $10}')
-    echo "$disk_dev Power on hours: $power_on_hours"
+    smart_info=$(smartctl -a $disk_dev)
+    # 检测是否支持power on hours
+    if echo "$smart_info" | grep -q "Power_On_Hours" ; then
+        power_on_hours=$(echo "$smart_info" | grep "Power_On_Hours" | awk '{print $10}')
+        echo "$disk_dev Power on hours: $power_on_hours"
+    else
+        echo "$disk_dev does not support Power on hours"
+    fi
 done
+
 
