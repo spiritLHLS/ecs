60823d6d570677787ccdfa4eb0773ef3d7e104ac
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-01-16 19:43:35 +0800

    Update disk_time.sh

--- archive/disk_time.sh
+++ archive/disk_time.sh
@@ -3,16 +3,31 @@
 
 # 检测所有硬盘通电时长
 
-# 获取所有硬盘设备文件
 disk_list=$(ls /dev/sd* | grep -v [0-9])
 
-# 循环检测每个硬盘
+#检测smartctl是否安装
+if ! command -v smartctl &> /dev/null
+then
+    echo "smartctl not found, installing smartctl..."
+    if [ -f /etc/redhat-release ] || [ -f /etc/centos-release ]; then
+        yum install smartmontools -y
+    elif [ -f /etc/debian_version ]; then
+        apt-get install smartmontools -y
+    elif [ -f /etc/fedora-release ]; then
+        dnf install smartmontools -y
+    elif [ -f /etc/arch-release ]; then
+        pacman -S smartmontools -y
+    elif [ -f /etc/alpine-release ]; then
+        apk add smartmontools
+    else
+        echo "Unsupported distribution, please install smartctl manually"
+        exit 1
+    fi
+fi
+
 for disk_dev in $disk_list
 do
-  # 使用smartctl命令查询硬盘信息
-  power_on_hours=$(smartctl -A $disk_dev | grep "Power_On_Hours" | awk '{print $10}')
-  
-  # 输出硬盘通电时长
-  echo "$disk_dev Power on hours: $power_on_hours"
+    power_on_hours=$(smartctl -A $disk_dev | grep "Power_On_Hours" | awk '{print $10}')
+    echo "$disk_dev Power on hours: $power_on_hours"
 done
 
