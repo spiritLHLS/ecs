517fb7eb69e8a9116282595cc24cd64af7a81fab
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-01-19 12:17:50 +0800

    Update foreverreturn.sh

--- foreverreturn.sh
+++ foreverreturn.sh
@@ -6,10 +6,66 @@ yellow(){ echo -e "\033[33m\033[01m$1\033[0m"; }
 green(){ echo -e "\033[32m\033[01m$1\033[0m"; }
 reading(){ read -rp "$(green "$1")" "$2"; }
 translate(){ [[ -n "$1" ]] && curl -ksm8 "http://fanyi.youdao.com/translate?&doctype=json&type=AUTO&i=${1//[[:space:]]/}" | cut -d \" -f18 2>/dev/null; }
+TEMP_FILE='ip.test'
 
 check_dependencies(){ for c in $@; do
 type -p $c >/dev/null 2>&1 || (yellow " 安装 $c 中…… " && ${PACKAGE_INSTALL[b]} "$c") || (yellow " 先升级软件库才能继续安装 \$c，时间较长，请耐心等待…… " && ${PACKAGE_UPDATE[b]} && ${PACKAGE_INSTALL[b]} "$c")
 ! type -p $c >/dev/null 2>&1 && yellow " 安装 \$c 失败，脚本中止，问题反馈:[https://github.com/fscarmen/tools/issues] " && exit 1; done; }
+
+fscarmen_route_script(){
+    rm -f $TEMP_FILE
+    local ARCHITECTURE="$(uname -m)"
+        case $ARCHITECTURE in
+        x86_64 )  local FILE=besttrace;;
+        aarch64 ) local FILE=besttracearm;;
+        i386 )    local FILE=besttracemac;;
+        * ) _red " 只支持 AMD64、ARM64、Mac 使用，问题反馈:[https://github.com/fscarmen/tools/issues] " && return;;
+        esac
+    curl -s -L -k "https://github.com/fscarmen/tools/raw/main/besttrace/${FILE}" -o $FILE && chmod +x $FILE &>/dev/null
+    green "依次测试电信，联通，移动经过的地区及线路，核心程序来由: ipip.net ，请知悉!" >> $TEMP_FILE
+    ./"$FILE" "$ip" -g cn | sed "s/^[ ]//g" | sed "/^[ ]/d" | sed '/ms/!d' | sed "s#.* \([0-9.]\+ ms.*\)#\1#g" >> $TEMP_FILE
+    cat $TEMP_FILE
+    rm -f $TEMP_FILE
+}
+
+main(){
+	ip=$1
+	green "\n 本脚说明：测 VPS ——> 对端 经过的地区及线路，填本地IP就是测回程，核心程序来自: https://www.ipip.net/ ，请知悉！"
+
+	[[ -z "$ip" || $ip = '[DESTINATION_IP]' ]] && reading "\n 请输入目的地 IP: " ip
+	yellow "\n 检测中，请稍等片刻。\n"
+
+	# 遍历本机可以使用的 IP API 服务商
+	API_NET=("api.ip.sb" "ifconfig.co")
+	API_URL=("api.ip.sb/geoip" "ifconfig.co/json")
+	API_ASN=("isp" "asn_org")
+	for ((p=0; p<${#API_NET[@]}; p++)); do ping -c1 -W1 ${API_NET[p]} >/dev/null 2>&1 && IP_API="${API_NET[p]}" && break; done
+
+	IP_4=$(curl -s4m5 -A Mozilla https://${API_URL[p]}) &&
+	WAN_4=$(expr "$IP_4" : '.*ip\":[ ]*\"\([^"]*\).*')
+	if [ -n "$WAN_4" ]; then
+	  COUNTRY_4E=$(expr "$IP_4" : '.*country\":[ ]*\"\([^"]*\).*')
+	  COUNTRY_4=$(translate "$COUNTRY_4E")
+	  ASNORG_4=$(expr "$IP_4" : '.*'${API_ASN[p]}'\":[ ]*\"\([^"]*\).*')
+	  TYPE_4=$(curl -4m5 -sSL https://www.abuseipdb.com/check/$WAN_4 2>/dev/null | grep -A2 '<th>Usage Type</th>' | tail -n 1 | sed "s@Data Center/Web Hosting/Transit@数据中心@;s@Fixed Line ISP@家庭宽带@;s@Commercial@商业宽带@;s@Mobile ISP@移动流量@;s@Content Delivery Network@内容分发网络(CDN)@;s@Search Engine Spider@搜索引擎蜘蛛@;s@University/College/School@教育网@;s@Unknown@未知@")
+	  green " IPv4: $WAN_4\t\t 地区: $COUNTRY_4\t 类型: $TYPE_4\t ASN: $ASNORG_4\n"
+	fi
+
+	IP_6=$(curl -s6m5 -A Mozilla https://${API_URL[p]}) &&
+	WAN_6=$(expr "$IP_6" : '.*ip\":[ ]*\"\([^"]*\).*') &&
+	if [ -n "$WAN_6" ]; then
+	  COUNTRY_6E=$(expr "$IP_6" : '.*country\":[ ]*\"\([^"]*\).*')
+	  COUNTRY_6=$(translate "$COUNTRY_6E")
+	  ASNORG_6=$(expr "$IP_6" : '.*'${API_ASN[p]}'\":[ ]*\"\([^"]*\).*')
+	  TYPE_6=$(curl -6m5 -sSL https://www.abuseipdb.com/check/$WAN_6 2>/dev/null | grep -A2 '<th>Usage Type</th>' | tail -n 1 | sed "s@Data Center/Web Hosting/Transit@数据中心@;s@Fixed Line ISP@家庭宽带@;s@Commercial@商业宽带@;s@Mobile ISP@移动流量@;s@Content Delivery Network@内容分发网络(CDN)@;s@Search Engine Spider@搜索引擎蜘蛛@;s@University/College/School@教育网@;s@Unknown@未知@")
+	  green " IPv6: $WAN_6\t 地区: $COUNTRY_6\t 类型: $TYPE_6\t ASN: $ASNORG_6\n"
+	fi
+
+	[[ $ip =~ '.' && -z "$IP_4" ]] && red " VPS 没有 IPv4 网络，不能查 $ip\n" && exit 1
+	[[ $ip =~ ':' && -z "$IP_6" ]] && red " VPS 没有 IPv6 网络，不能查 $ip\n" && exit 1
+
+	fscarmen_route_script
+}
 
 ARCHITECTURE="$(arch)"
 case $ARCHITECTURE in
@@ -25,7 +81,6 @@ if [[ $ARCHITECTURE = i386 ]]; then
   b=0
   SYSTEM='macOS'
   PACKAGE_INSTALL=("brew install")
-  
 else
   CMD=(	"$(grep -i pretty_name /etc/os-release 2>/dev/null | cut -d \" -f2)"
       	"$(hostnamectl 2>/dev/null | grep -i system | cut -d : -f2)"
@@ -52,50 +107,6 @@ fi
 [[ -z $SYSTEM ]] && red " 本脚本只支持 Debian、Ubuntu、CentOS、Alpine 或者 macOS 系统,问题反馈:[https://github.com/fscarmen/warp_unlock/issues] " && exit 1
 
 check_dependencies curl sudo
-
-main(){
-ip=$1
-green "\n 本脚说明：测 VPS ——> 对端 经过的地区及线路，填本地IP就是测回程，核心程序来自: https://www.ipip.net/ ，请知悉！"
-
-[[ -z "$ip" || $ip = '[DESTINATION_IP]' ]] && reading "\n 请输入目的地 IP: " ip
-yellow "\n 检测中，请稍等片刻。\n"
-
-IP_4=$(curl -s4m5 https://api.ipify.org) &&
-WAN_4=$(expr "$IP_4" : '.*ip\":[ ]*\"\([^"]*\).*')
-COUNTRY_4E=$(expr "$IP_4" : '.*country\":[ ]*\"\([^"]*\).*') &&
-if [ -n "$WAN_4" ]; then
-COUNTRY_4=$(translate "$COUNTRY_4E") &&
-  COUNTRY_4E=$(expr "$IP_4" : '.*country\":[ ]*\"\([^"]*\).*')
-ASNORG_4=$(expr "$IP_4" : '.*'${API_ASN[p]}'\":[ ]*\"\([^"]*\).*')
-  COUNTRY_4=$(translate "$COUNTRY_4E")
-[ -n "$WAN_4" ] && green " IPv4: $WAN_4\t\t 地区: $COUNTRY_4\t ASN: $ASNORG_4\n"
-  ASNORG_4=$(expr "$IP_4" : '.*'${API_ASN[p]}'\":[ ]*\"\([^"]*\).*')
-#PE_4=$(curl -sm5 ping.pe/$WAN_4) &&
-  TYPE_4=$(curl -4m5 -sSL https://www.abuseipdb.com/check/$WAN_4 2>/dev/null | grep -A2 '<th>Usage Type</th>' | tail -n 1 | sed "s#Data Center/Web Hosting/Transit#数据中心#;s#Fixed Line ISP#家庭宽带#;s#Commercial#商业宽带#;s#Mobile ISP#移动流量#;s#Unknown#未知#")
-#COOKIE_4=$(echo $PE_4 | sed "s/.*document.cookie=\"\([^;]\{1,\}\).*/\1/g") &&
-  green " IPv4: $WAN_4\t\t 地区: $COUNTRY_4\t 类型: $TYPE_4\t ASN: $ASNORG_4\n"
-#TYPE_4=$(curl -sm5 --header "cookie: $COOKIE_4" ping.pe/$WAN_4 | grep "id='page-div'" | sed "s/.*\[\(.*\)\].*/\1/g" | sed "s/.*orange'>\([^<]\{1,\}\).*/\1/g" | sed "s/hosting/数据中心/g;s/residential/家庭宽带/g") &&
-fi
-  
-IP_6=$(curl -s6m5 https://api.ipify.org) &&
-WAN_6=$(expr "$IP_6" : '.*ip\":\"\([^"]*\).*') &&
-if [ -n "$WAN_6" ]; then
-  COUNTRY_6E=$(expr "$IP_6" : '.*country\":[ ]*\"\([^"]*\).*')
-  COUNTRY_6=$(translate "$COUNTRY_6E")
-  ASNORG_6=$(expr "$IP_6" : '.*'${API_ASN[p]}'\":[ ]*\"\([^"]*\).*')
-  TYPE_6=$(curl -6m5 -sSL https://www.abuseipdb.com/check/$WAN_6 2>/dev/null | grep -A2 '<th>Usage Type</th>' | tail -n 1 | sed "s#Data Center/Web Hosting/Transit#数据中心#;s#Fixed Line ISP#家庭宽带#;s#Commercial#商业宽带#;s#Mobile ISP#移动流量#;s#Unknown#未知#")
-  green " IPv6: $WAN_6\t 地区: $COUNTRY_6\t 类型: $TYPE_6\t ASN: $ASNORG_6\n"
-fi
-
-
-[[ $ip =~ '.' && -z "$IP_4" ]] && red " VPS 没有 IPv4 网络，不能查 $ip\n" && exit 1
-[[ $ip =~ ':' && -z "$IP_6" ]] && red " VPS 没有 IPv6 网络，不能查 $ip\n" && exit 1
-
-curl -s -L -k https://github.com/fscarmen/tools/raw/main/besttrace/$FILE -o $FILE && chmod +x $FILE &>/dev/null
-sudo ./"$FILE" "$ip" -g cn | sed "s/^[ ]//g" | sed "/^[ ]/d" | sed '/ms/!d' | sed "s#.* \([0-9.]\+ ms.*\)#\1#g" >> $TEMP_FILE
-green "$(cat $TEMP_FILE)"
-rm -f $TEMP_FILE
-}
 
 while [ "1" = "1" ]
   do
