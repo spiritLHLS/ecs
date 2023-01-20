e8db7ec68b8bb3737738ad3a4b6c42c45f1e93e9
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-01-20 17:38:23 +0800

    Create nexttrace.sh

---
+++ archive/nexttrace.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+# by https://github.com/spiritLHLS/ecs
+# 2023.01.20
+
+#!/usr/bin/env bash
+_red() { echo -e "\033[31m\033[01m$@\033[0m"; }
+_green() { echo -e "\033[32m\033[01m$@\033[0m"; }
+_yellow() { echo -e "\033[33m\033[01m$@\033[0m"; }
+_blue() { echo -e "\033[36m\033[01m$@\033[0m"; }
+reading(){ read -rp "$(_green "$1")" "$2"; }
+translate(){ [[ -n "$1" ]] && curl -ksm8 "http://fanyi.youdao.com/translate?&doctype=json&type=AUTO&i=${1//[[:space:]]/}" | cut -d \" -f18 2>/dev/null; }
+TEMP_FILE='ip.test'
+
+check_dependencies(){ for c in $@; do
+type -p $c >/dev/null 2>&1 || (_yellow " 安装 $c 中…… " && ${PACKAGE_INSTALL[b]} "$c") || (_yellow " 先升级软件库才能继续安装 \$c，时间较长，请耐心等待…… " && ${PACKAGE_UPDATE[b]} && ${PACKAGE_INSTALL[b]} "$c")
+! type -p $c >/dev/null 2>&1 && _yellow " 安装 \$c 失败，脚本中止，问题反馈:[https://github.com/fscarmen/tools/issues] " && exit 1; done; }
+
+ARCHITECTURE="$(arch)"
+# 多方式判断操作系统，试到有值为止。只支持 Debian 10/11、Ubuntu 18.04/20.04 或 CentOS 7/8 ,如非上述操作系统，退出脚本
+if [[ $ARCHITECTURE = i386 ]]; then
+  sw_vesrs 2>/dev/null | grep -qvi macos && _red " 本脚本只支持 Debian、Ubuntu、CentOS、Alpine 或者 macOS 系统,问题反馈:[https://github.com/fscarmen/warp_unlock/issues] " && exit 1
+  b=0
+  SYSTEM='macOS'
+  PACKAGE_INSTALL=("brew install")
+else
+  CMD=(	"$(grep -i pretty_name /etc/os-release 2>/dev/null | cut -d \" -f2)"
+      	"$(hostnamectl 2>/dev/null | grep -i system | cut -d : -f2)"
+	"$(lsb_release -sd 2>/dev/null)"
+	"$(grep -i description /etc/lsb-release 2>/dev/null | cut -d \" -f2)"
+	"$(grep . /etc/redhat-release 2>/dev/null)"
+	"$(grep . /etc/issue 2>/dev/null | cut -d \\ -f1 | sed '/^[ ]*$/d')"
+	)
+
+  REGEX=("debian" "ubuntu" "centos|red hat|kernel|oracle linux|amazon linux|alma|rocky")
+  RELEASE=("Debian" "Ubuntu" "CentOS")
+  PACKAGE_UPDATE=("apt -y update" "apt -y update" "yum -y update")
+  PACKAGE_INSTALL=("apt -y install" "apt -y install" "yum -y install")
+
+  for a in "${CMD[@]}"; do
+	  SYS="$a" && [[ -n $SYS ]] && break
+  done
+  
+  for ((b=0; b<${#REGEX[@]}; b++)); do
+	[[ $(echo "$SYS" | tr '[:upper:]' '[:lower:]') =~ ${REGEX[b]} ]] && SYSTEM="${RELEASE[b]}" && break
+  done
+fi
+
+[[ -z $SYSTEM ]] && _red " 本脚本只支持 Debian、Ubuntu、CentOS、Alpine 或者 macOS 系统,问题反馈:[https://github.com/fscarmen/warp_unlock/issues] " && exit 1
+
+check_dependencies curl sudo
+
+# 安装配件
+bash <(curl -Ls https://raw.githubusercontent.com/sjlleo/nexttrace/main/nt_install.sh)
+
+# 头部信息
+_green "\n使用前 nexttrace 请务必放低预期，如果追求的是数据的精确，请选择 besttrace"
+_green "原始仓库说明：https://github.com/sjlleo/nexttrace/blob/main/README_zh_CN.md"
+_green "本人仅制作了shell脚本，具体核心程序来源于上面的原始仓库"
+# 读取IP
+while true; do
+    [[ -z "$ip" || $ip = '[DESTINATION_IP]' ]] && reading "\n请输入目的地 IP或网址: " ip
+    if [[ $ip =~ ^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$ ]] || [[ $ip =~ ^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:))$ ]] || (echo $ip | grep -E -q '^(http|https|ftp|file)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]'); then
+        break
+    fi
+    echo "\n请输入有效的 IP 或网址"
+done
+
+reading "\n需要顺便输出路径可视化的图片链接吗？([y]/n) " confirm
+if [ "$confirm" == "n" ]; then
+  _green "\nICMP Trace & MAP:"
+	nexttrace --map "$ip" 
+else
+	_green "\nICMP Trace:"
+	nexttrace "$ip"
+fi
