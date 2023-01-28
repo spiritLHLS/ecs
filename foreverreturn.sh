34c48b333b0122c026e8e9f505ac9d9243932dd3
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-01-28 09:09:25 +0800

    Update foreverreturn.sh

--- foreverreturn.sh
+++ foreverreturn.sh
@@ -41,7 +41,7 @@ main(){
 	API_ASN=("isp" "asn_org")
 	for ((p=0; p<${#API_NET[@]}; p++)); do ping -c1 -W1 ${API_NET[p]} >/dev/null 2>&1 && IP_API="${API_NET[p]}" && break; done
 
-	IP_4=$(curl -s4m5 -A Mozilla https://${API_URL[p]}) &&
+	IP_4=$ip &&
 	WAN_4=$(expr "$IP_4" : '.*ip\":[ ]*\"\([^"]*\).*')
 	if [ -n "$WAN_4" ]; then
 	  COUNTRY_4E=$(expr "$IP_4" : '.*country\":[ ]*\"\([^"]*\).*')
@@ -51,18 +51,18 @@ main(){
 	  green " IPv4: $WAN_4\t\t 地区: $COUNTRY_4\t 类型: $TYPE_4\t ASN: $ASNORG_4\n"
 	fi
 
-	IP_6=$(curl -s6m5 -A Mozilla https://${API_URL[p]}) &&
-	WAN_6=$(expr "$IP_6" : '.*ip\":[ ]*\"\([^"]*\).*') &&
-	if [ -n "$WAN_6" ]; then
-	  COUNTRY_6E=$(expr "$IP_6" : '.*country\":[ ]*\"\([^"]*\).*')
-	  COUNTRY_6=$(translate "$COUNTRY_6E")
-	  ASNORG_6=$(expr "$IP_6" : '.*'${API_ASN[p]}'\":[ ]*\"\([^"]*\).*')
-	  TYPE_6=$(curl -6m5 -sSL https://www.abuseipdb.com/check/$WAN_6 2>/dev/null | grep -A2 '<th>Usage Type</th>' | tail -n 1 | sed "s@Data Center/Web Hosting/Transit@数据中心@;s@Fixed Line ISP@家庭宽带@;s@Commercial@商业宽带@;s@Mobile ISP@移动流量@;s@Content Delivery Network@内容分发网络(CDN)@;s@Search Engine Spider@搜索引擎蜘蛛@;s@University/College/School@教育网@;s@Unknown@未知@")
-	  green " IPv6: $WAN_6\t 地区: $COUNTRY_6\t 类型: $TYPE_6\t ASN: $ASNORG_6\n"
-	fi
+# 	IP_6=$(curl -s6m5 -A Mozilla https://${API_URL[p]}) &&
+# 	WAN_6=$(expr "$IP_6" : '.*ip\":[ ]*\"\([^"]*\).*') &&
+# 	if [ -n "$WAN_6" ]; then
+# 	  COUNTRY_6E=$(expr "$IP_6" : '.*country\":[ ]*\"\([^"]*\).*')
+# 	  COUNTRY_6=$(translate "$COUNTRY_6E")
+# 	  ASNORG_6=$(expr "$IP_6" : '.*'${API_ASN[p]}'\":[ ]*\"\([^"]*\).*')
+# 	  TYPE_6=$(curl -6m5 -sSL https://www.abuseipdb.com/check/$WAN_6 2>/dev/null | grep -A2 '<th>Usage Type</th>' | tail -n 1 | sed "s@Data Center/Web Hosting/Transit@数据中心@;s@Fixed Line ISP@家庭宽带@;s@Commercial@商业宽带@;s@Mobile ISP@移动流量@;s@Content Delivery Network@内容分发网络(CDN)@;s@Search Engine Spider@搜索引擎蜘蛛@;s@University/College/School@教育网@;s@Unknown@未知@")
+# 	  green " IPv6: $WAN_6\t 地区: $COUNTRY_6\t 类型: $TYPE_6\t ASN: $ASNORG_6\n"
+# 	fi
 
 	[[ $ip =~ '.' && -z "$IP_4" ]] && red " VPS 没有 IPv4 网络，不能查 $ip\n" && exit 1
-	[[ $ip =~ ':' && -z "$IP_6" ]] && red " VPS 没有 IPv6 网络，不能查 $ip\n" && exit 1
+# 	[[ $ip =~ ':' && -z "$IP_6" ]] && red " VPS 没有 IPv6 网络，不能查 $ip\n" && exit 1
 
 	fscarmen_route_script
 }
