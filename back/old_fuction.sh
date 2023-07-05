2f31c0ecbce2dadfcd93b77d49534a49efbc0be1
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2023-07-05 17:29:21 +0800

    Update old_fuction.sh

--- back/old_fuction.sh
+++ back/old_fuction.sh
@@ -1,3 +1,95 @@
+# SystemInfo_GetCPUInfo() {
+#     mkdir -p ${WorkDir}/data >/dev/null 2>&1
+#     if [ -f "/proc/cpuinfo" ]; then
+#         cat /proc/cpuinfo >${WorkDir}/data/cpuinfo
+#         local ReadCPUInfo="cat ${WorkDir}/data/cpuinfo"
+#         LBench_Result_CPUModelName="$($ReadCPUInfo | awk -F ': ' '/model name/{print $2}' | sort -u)"
+#         local CPUFreqCount="$($ReadCPUInfo | awk -F ': ' '/cpu MHz/{print $2}' | sort -run | wc -l)"
+#         if [ "${CPUFreqCount}" -ge "2" ]; then
+#             local CPUFreqArray="$(cat /proc/cpuinfo | awk -F ': ' '/cpu MHz/{print $2}' | sort -run)"
+#             local CPUFreq_Min="$(echo "$CPUFreqArray" | grep -oE '[0-9]+.[0-9]{3}' | awk 'BEGIN {min = 2147483647} {if ($1+0 < min+0) min=$1} END {print min}')"
+#             local CPUFreq_Max="$(echo "$CPUFreqArray" | grep -oE '[0-9]+.[0-9]{3}' | awk 'BEGIN {max = 0} {if ($1+0 > max+0) max=$1} END {print max}')"
+#             LBench_Result_CPUFreqMinGHz="$(echo $CPUFreq_Min | awk '{printf "%.2f\n",$1/1000}')"
+#             LBench_Result_CPUFreqMaxGHz="$(echo $CPUFreq_Max | awk '{printf "%.2f\n",$1/1000}')"
+#             Flag_DymanicCPUFreqDetected="1"
+#         else
+#             LBench_Result_CPUFreqMHz="$($ReadCPUInfo | awk -F ': ' '/cpu MHz/{print $2}' | sort -u)"
+#             LBench_Result_CPUFreqGHz="$(echo $LBench_Result_CPUFreqMHz | awk '{printf "%.2f\n",$1/1000}')"
+#             Flag_DymanicCPUFreqDetected="0"
+#         fi
+#         LBench_Result_CPUCacheSize="$($ReadCPUInfo | awk -F ': ' '/cache size/{print $2}' | sort -u)"
+#         LBench_Result_CPUPhysicalNumber="$($ReadCPUInfo | awk -F ': ' '/physical id/{print $2}' | sort -u | wc -l)"
+#         LBench_Result_CPUCoreNumber="$($ReadCPUInfo | awk -F ': ' '/cpu cores/{print $2}' | sort -u)"
+#         LBench_Result_CPUThreadNumber="$($ReadCPUInfo | awk -F ': ' '/cores/{print $2}' | wc -l)"
+#         LBench_Result_CPUProcessorNumber="$($ReadCPUInfo | awk -F ': ' '/processor/{print $2}' | wc -l)"
+#         LBench_Result_CPUSiblingsNumber="$($ReadCPUInfo | awk -F ': ' '/siblings/{print $2}' | sort -u)"
+#         LBench_Result_CPUTotalCoreNumber="$($ReadCPUInfo | awk -F ': ' '/physical id/&&/0/{print $2}' | wc -l)"
+        
+#         # 虚拟化能力检测
+#         SystemInfo_GetVirtType
+#         if [ "${Var_VirtType}" = "dedicated" ] || [ "${Var_VirtType}" = "wsl" ]; then
+#             LBench_Result_CPUIsPhysical="1"
+#             local VirtCheck="$(cat /proc/cpuinfo | grep -oE 'vmx|svm' | uniq)"
+#             if [ "${VirtCheck}" != "" ]; then
+#                 LBench_Result_CPUVirtualization="1"
+#                 local VirtualizationType="$(lscpu | awk /Virtualization:/'{print $2}')"
+#                 LBench_Result_CPUVirtualizationType="${VirtualizationType}"
+#             else
+#                 LBench_Result_CPUVirtualization="0"
+#             fi
+#         elif [ "${Var_VirtType}" = "kvm" ] || [ "${Var_VirtType}" = "hyperv" ] || [ "${Var_VirtType}" = "microsoft" ] || [ "${Var_VirtType}" = "vmware" ]; then
+#             LBench_Result_CPUIsPhysical="0"
+#             local VirtCheck="$(cat /proc/cpuinfo | grep -oE 'vmx|svm' | uniq)"
+#             if [ "${VirtCheck}" = "vmx" ] || [ "${VirtCheck}" = "svm" ]; then
+#                 LBench_Result_CPUVirtualization="2"
+#                 local VirtualizationType="$(lscpu | awk /Virtualization:/'{print $2}')"
+#                 LBench_Result_CPUVirtualizationType="${VirtualizationType}"
+#             else
+#                 LBench_Result_CPUVirtualization="0"
+#             fi        
+#         else
+#             LBench_Result_CPUIsPhysical="0"
+#         fi
+#     else
+#         $sysctl_path -a >${WorkDir}/data/sysctl_info
+#         local ReadCPUInfo="cat ${WorkDir}/data/sysctl_info"
+#         LBench_Result_CPUModelName="$($ReadCPUInfo 2>/dev/null | awk -F ': ' '/hw.model/{print $2}' | sort -u)"
+#         LBench_Result_CPUFreqMHz="$($ReadCPUInfo 2>/dev/null | awk -F ': ' '/dev.cpu.0.freq/{print $2}')"
+#         LBench_Result_CPUCacheSize="$($ReadCPUInfo 2>/dev/null | awk -F ': ' '/hw.cacheconfig/{print $2}')"
+#         LBench_Result_CPUFreqGHz="$(echo $LBench_Result_CPUFreqMHz | awk '{printf "%.2f\n",$1/1000}')"
+#         LBench_Result_CPUPhysicalNumber="$($ReadCPUInfo 2>/dev/null | awk -F ': ' '/hw.physicalcpu/{print $2}')"
+#         LBench_Result_CPUCoreNumber="$($ReadCPUInfo 2>/dev/null | awk -F ': ' '/hw.ncpu/{print $2}')"
+#         LBench_Result_CPUThreadNumber="$($ReadCPUInfo 2>/dev/null | awk -F ': ' '/hw.ncpu/{print $2}')"
+#         LBench_Result_CPUProcessorNumber="$($ReadCPUInfo 2>/dev/null | awk -F ': ' '/hw.ncpu/{print $2}')"
+#         LBench_Result_CPUSiblingsNumber="$($ReadCPUInfo 2>/dev/null | awk -F ': ' '/hw.smt/{print $2}')"
+#         LBench_Result_CPUTotalCoreNumber="$($ReadCPUInfo 2>/dev/null | awk -F ': ' '/hw.ncpu/{print $2}')"
+
+#         # 虚拟化能力检测
+#         SystemInfo_GetVirtType
+#         if [ "${Var_VirtType}" = "dedicated" ] || [ "${Var_VirtType}" = "wsl" ]; then
+#             LBench_Result_CPUIsPhysical="1"
+#             local VirtCheck="$($sysctl_path -a | grep -E 'hw.vmx|hw.svm' | uniq)"
+#             if [ "${VirtCheck}" != "" ]; then
+#                 LBench_Result_CPUVirtualization="1"
+#                 LBench_Result_CPUVirtualizationType="Native"
+#             else
+#                 LBench_Result_CPUVirtualization="0"
+#             fi
+#         elif [ "${Var_VirtType}" = "kvm" ] || [ "${Var_VirtType}" = "hyperv" ] || [ "${Var_VirtType}" = "microsoft" ] || [ "${Var_VirtType}" = "vmware" ]; then
+#             LBench_Result_CPUIsPhysical="0"
+#             local VirtCheck="$($sysctl_path -a | grep -E 'hw.vmx|hw.svm' | uniq)"
+#             if [ "${VirtCheck}" != "" ]; then
+#                 LBench_Result_CPUVirtualization="2"
+#                 LBench_Result_CPUVirtualizationType="${Var_VirtType}"
+#             else
+#                 LBench_Result_CPUVirtualization="0"
+#             fi
+#         else
+#             LBench_Result_CPUIsPhysical="0"
+#         fi
+#     fi
+# }
+
 # get_longest_first_element() {
 #     # 获取一个列表中最长的元素 - 信息最多的
 #     local list=("$@")
