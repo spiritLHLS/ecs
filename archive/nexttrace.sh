a1028075d16d004ee4a1f7f8475a0d8866f9ff30
fscarmen <62703343+fscarmen@users.noreply.github.com>
2023-08-19 17:46:37 +0800

    用正则处理 nexttrace 的输出显示
    ```
    使用 nexttrace 前请务必放低预期，如果追求的是数据的精确，请选择 besttrace
    原始仓库说明：https://github.com/sjlleo/nexttrace/blob/main/README_zh_CN.md
    本人仅制作了shell脚本，具体核心程序来源于上面的原始仓库
    
    请输入目的地 IP或网址: 114.114.114.114
    11.05 ms        AS15169 美国 伊利诺伊州 芝加哥 google.com
    11.23 ms        AS6453 美国 伊利诺伊州 芝加哥 tatacommunications.com
    27.45 ms        AS6453 美国 伊利诺伊州 芝加哥 tatacommunications.com
    27.60 ms        AS6453 美国 纽约州 纽约 tatacommunications.com
    28.17 ms        AS174 [COGENT-BONE] 美国 纽约州 纽约 cogentco.com
    27.94 ms        AS174 [COGENT-BONE] 美国 纽约州 纽约 cogentco.com
    39.93 ms        AS174 [COGENT-BONE] 美国 俄亥俄州 克里夫兰 cogentco.com
    42.08 ms        AS174 [COGENT-BONE] 美国 伊利诺伊州 芝加哥 cogentco.com
    45.91 ms        AS174 [COGENT-BONE] 美国 伊利诺伊州 芝加哥 cogentco.com
    51.49 ms        AS174 美国 伊利诺伊州 芝加哥 cogentco.com
    45.91 ms        * [XFInfo] 114DNS.COM 114DNS.COM 美国Cogent通信公司
    ```

--- archive/nexttrace.sh
+++ archive/nexttrace.sh
@@ -16,41 +16,47 @@ type -p $c >/dev/null 2>&1 || (_yellow " 安装 $c 中…… " && ${PACKAGE_INST
 ! type -p $c >/dev/null 2>&1 && _yellow " 安装 \$c 失败，脚本中止，问题反馈:[https://github.com/fscarmen/tools/issues] " && exit 1; done; }
 
 ARCHITECTURE="$(arch)"
-# 多方式判断操作系统，试到有值为止。只支持 Debian 10/11、Ubuntu 18.04/20.04 或 CentOS 7/8 ,如非上述操作系统，退出脚本
-if [[ $ARCHITECTURE = i386 ]]; then
-  sw_vesrs 2>/dev/null | grep -qvi macos && _red " 本脚本只支持 Debian、Ubuntu、CentOS、Alpine 或者 macOS 系统,问题反馈:[https://github.com/fscarmen/warp_unlock/issues] " && exit 1
-  b=0
-  SYSTEM='macOS'
-  PACKAGE_INSTALL=("brew install")
-else
-  CMD=(	"$(grep -i pretty_name /etc/os-release 2>/dev/null | cut -d \" -f2)"
-      	"$(hostnamectl 2>/dev/null | grep -i system | cut -d : -f2)"
-	"$(lsb_release -sd 2>/dev/null)"
-	"$(grep -i description /etc/lsb-release 2>/dev/null | cut -d \" -f2)"
-	"$(grep . /etc/redhat-release 2>/dev/null)"
-	"$(grep . /etc/issue 2>/dev/null | cut -d \\ -f1 | sed '/^[ ]*$/d')"
-	)
+case "$ARCHITECTURE" in
+  "x86_64" | "amd64" )
+    FILE=nexttrace_linux_amd64
+    ;;
+  "armv7l" | "armv8" | "armv8l" | "aarch64")
+    FILE=nexttrace_linux_arm64
+    ;;
+  "i386" | "i686")
+    FILE=nexttrace_darwin_amd64
+    ;;
+  * )
+    red " 本脚本只支持 AMD64、ARM64、i386 和 i686 使用，问题反馈:[https://github.com/fscarmen/tools/issues] " && exit 1
+esac
 
-  REGEX=("debian" "ubuntu" "centos|red hat|kernel|oracle linux|amazon linux|alma|rocky")
-  RELEASE=("Debian" "Ubuntu" "CentOS")
-  PACKAGE_UPDATE=("apt -y update" "apt -y update" "yum -y update")
-  PACKAGE_INSTALL=("apt -y install" "apt -y install" "yum -y install")
-
-  for a in "${CMD[@]}"; do
-	  SYS="$a" && [[ -n $SYS ]] && break
-  done
-  
-  for ((b=0; b<${#REGEX[@]}; b++)); do
-	[[ $(echo "$SYS" | tr '[:upper:]' '[:lower:]') =~ ${REGEX[b]} ]] && SYSTEM="${RELEASE[b]}" && break
-  done
+# 多方式判断操作系统，试到有值为止。只支持 Debian、Ubuntu 或 CentOS， 如非上述操作系统，退出脚本
+if [ -s /etc/os-release ]; then
+  SYS="$(grep -i pretty_name /etc/os-release | cut -d \" -f2)"
+elif [ $(type -p hostnamectl) ]; then
+  SYS="$(hostnamectl | grep -i system | cut -d : -f2)"
+elif [ $(type -p lsb_release) ]; then
+  SYS="$(lsb_release -sd)"
+elif [ -s /etc/lsb-release ]; then
+  SYS="$(grep -i description /etc/lsb-release | cut -d \" -f2)"
+elif [ -s /etc/redhat-release ]; then
+  SYS="$(grep . /etc/redhat-release)"
+elif [ -s /etc/issue ]; then
+  SYS="$(grep . /etc/issue | cut -d '\' -f1 | sed '/^[ ]*$/d')"
 fi
+REGEX=("debian" "ubuntu" "centos|red hat|kernel|oracle linux|amazon linux|alma|rocky")
+RELEASE=("Debian" "Ubuntu" "CentOS")
+PACKAGE_UPDATE=("apt -y update" "apt -y update" "yum -y update")
+PACKAGE_INSTALL=("apt -y install" "apt -y install" "yum -y install")
+for ((b=0; b<${#REGEX[@]}; b++)); do
+  [[ $(echo "$SYS" | tr '[:upper:]' '[:lower:]') =~ ${REGEX[b]} ]] && SYSTEM="${RELEASE[b]}" && break
+done
 
-[[ -z $SYSTEM ]] && _red " 本脚本只支持 Debian、Ubuntu、CentOS、Alpine 或者 macOS 系统,问题反馈:[https://github.com/fscarmen/warp_unlock/issues] " && exit 1
+[ -z "$SYSTEM" ] && _red " 本脚本只支持 Debian、Ubuntu 或者 CentOS 系统,问题反馈:[https://github.com/fscarmen/tools/issues] " && exit 1
 
 check_dependencies curl sudo
 clear
-# 安装配件
-bash <(curl -Ls https://raw.githubusercontent.com/sjlleo/nexttrace/main/nt_install.sh)
+
 # 头部信息
 _green "\n使用 nexttrace 前请务必放低预期，如果追求的是数据的精确，请选择 besttrace"
 _green "原始仓库说明：https://github.com/sjlleo/nexttrace/blob/main/README_zh_CN.md"
@@ -61,14 +67,25 @@ while true; do
     if [[ $ip =~ ^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$ ]] || [[ $ip =~ ^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:))$ ]] || (echo $ip | grep -E -q '^(http|https|ftp|file)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]'); then
         break
     fi
-    echo "\n请输入有效的 IP 或网址"
+    echo "请输入有效的 IP 或网址"
 done
 
-reading "\n需要顺便输出路径可视化的图片链接吗？([y]/n) " confirm
-if [ "$confirm" == "n" ]; then
-  _green "\nICMP Trace:"
-  nexttrace "$ip"
-else
-  _green "\nICMP Trace & MAP:"
-  nexttrace --map "$ip"
+# 下载 nexttrace 主程序
+if [ ! -e $FILE ]; then
+  VERSION=$(curl -sSL "https://api.github.com/repos/nxtrace/Ntrace-core/releases/latest" | awk -F \" '/tag_name/{print $4}')
+  curl -sLO https://github.com/nxtrace/Ntrace-core/releases/download/$VERSION/$FILE
+  chmod +x "$FILE" >/dev/null 2>&1
 fi
+
+# 查路由
+RESULT=$(./"$FILE" "$ip" -g cn 2>/dev/null)
+PART_1=$(echo "$RESULT" | grep '^[0-9]\{1,2\}[ ]\+[0-9a-f]' | awk '{$1="";$2="";print}' | sed "s@^[ ]\+@@g")
+PART_2=$(echo "$RESULT" | grep '\(.*ms\)\{3\}' | sed 's/.* \([0-9*].*ms\).*ms.*ms/\1/g')
+SPACE=' '
+for ((i=1; i<=$(echo "$PART_1" | wc -l); i++)); do
+  [ "$i" -eq 10 ] && unset SPACE
+  _green "$(echo "$PART_2" | sed -n "${i}p")\t$(echo "$PART_1" | sed -n "${i}p")"
+done
+
+# 执行完成，删除 nexttrace 主程序
+rm -f $FILE
