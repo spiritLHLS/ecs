3684d35a072002f8be6793ececdba1574cd2e953
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2023-12-04 23:03:58 +0800

    Update eo6s.sh

--- archive/eo6s.sh
+++ archive/eo6s.sh
@@ -18,12 +18,15 @@ done
 ${PACKAGE_INSTALL[int]} net-tools
 interface=$(ls /sys/class/net/ | grep -v "$(ls /sys/devices/virtual/net/)")
 current_ipv6=$(curl -s -6 -m 6 ipv6.ip.sb)
+echo ${current_ipv6}
 new_ipv6="${current_ipv6%:*}:3"
 ip addr add ${new_ipv6}/128 dev ${interface}
 sleep 1
 updated_ipv6=$(curl -s -6 -m 6 ipv6.ip.sb)
+echo ${updated_ipv6}
 ip addr del ${new_ipv6}/128 dev ${interface}
 final_ipv6=$(curl -s -6 -m 6 ipv6.ip.sb)
+echo ${final_ipv6}
 ipv6_prefixlen=""
 output=$(ifconfig ${interface} | grep -oP 'inet6 [^f][^e][^8][^0].*prefixlen \K\d+')
 num_lines=$(echo "$output" | wc -l)
@@ -33,8 +36,8 @@ else
     ipv6_prefixlen=$(echo "$output" | head -n 1)
 fi
 if [ "$updated_ipv6" == "$current_ipv6" ]; then
-    echo " IPV6 子网掩码     : 128"
+    echo "IPV6 子网掩码: 128"
 else
-    echo " IPV6 子网掩码     : $ipv6_prefixlen"
+    echo "IPV6 子网掩码: $ipv6_prefixlen"
 fi
 
